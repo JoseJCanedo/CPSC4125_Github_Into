ab9b825f67d7ed52d8115b7b9e9d8d23b1f61701
JoseJCanedo <canedo_jose@columbusstate.edu>
2020-08-27 22:53:13 +0000

    added js

--- functions.js
+++ functions.js
@@ -4,4 +4,49 @@ $(document).ready(function() {
     //     $(".test").html("<b>Hello world!</b>");
     // })
 
+    const a; //doesnt change
+    var b = 1; //can change larger scope
+    let c = 2; //can change smaller scope
+
+    if( b == 2) { //falsy == !=
+
+    } else if ( c === 3){ //same type === !==
+
+    } else {
+
+    }
+
+    var d = b==2 && c ===1?"yes": a != null? "yes": "no"; //ternary  condition ? true: false return(ternary)
+
+    //basic arithmatic: + - x /, modulus, b+1 =11 not always going to be 2 
+
+    //string, number, array [], json {}
+
+    //array[]
+    //sort, forEach, push, pop, shift 
+
+    var xkcdJson = {title: "Standard Model Changes", 
+                    day: "26"};
+    var xkcdDay = xkcdJson.day
+
+    //GET, PUT, POST, DELETE | CREATE, RETRIEVE, UPDATE, DELETE (CRUD)
+
+    for(var i =0; i < something; i++){
+
+    }
+    something.forEach()
+
+    function firstFunction(variables){
+        //process input,
+        return variables;
+        //return something
+    }
+
+    Promise(function(){
+        setTimeout(x);
+        //what youre waiting on
+    });
+
+    console.log(variableName);
+
 });
